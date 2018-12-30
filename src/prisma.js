286bc5d7b1e5959f09047f791aac78be1fa97581
Wayne Barlow <wbarlow@gmail.com>
2018-12-30 16:37:10 -0500

    add some code to mess around with the prisma bindings

--- src/prisma.js
+++ src/prisma.js
@@ -11,11 +11,43 @@ const prisma = new Prisma({
 // prisma.subscription
 // prisma.exists
 
-prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
-})
+// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
 
-prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
-})
+// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
 
+// prisma.mutation.createPost({ 
+//     data: { 
+//         title: "A second post from node", 
+//         body: "...",
+//         published: false,
+//         author: {
+//             connect: {
+//                 id: "cjqb6tfyi001c0720svt9anme"
+//             }
+//         }
+//     }
+// }, '{ id title body published }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+//     return prisma.query.users(null, '{ id name posts { id title } }')
+// }).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+
+prisma.mutation.updatePost({
+    data: {
+        body: "my updated text",
+        published: true
+    },
+    where: {
+        id: "cjqbekrxd002b0720ftsdfu1k"
+    }
+}, '{ id title body published }').then((data) => {
+    console.log(data)
+    return prisma.query.posts(null, '{ id title body published }')
+}).then((data) => {
+        console.log(JSON.stringify(data, undefined, 2))
+})
